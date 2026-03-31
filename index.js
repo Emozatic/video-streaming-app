0047cb2099f3c39fb6d5d183e63d7ac15cf7b21d
Lucky Sah <lk4179869@gmail.com>
2026-03-31 12:50:24 +0530

    adding error handling middleware with asyncWrap

--- index.js
+++ index.js
@@ -23,57 +23,62 @@ main()
 .then(()=>{console.log("connected to db")})
 .catch((err)=>{console.log(err)});
 
-
+function asyncWrap(fn){
+    return function(req,res,next){
+        fn(req,res,next).catch(err=>next(err));
+    }
+}
 
 //index route
-app.get("/home",async(req,res)=>{
+app.get("/home",asyncWrap(async(req,res)=>{
     let allVideos=await Video.find({});
     res.render("home.ejs",{allVideos});
-});
+}));
 
 //new route
-app.get("/home/new",(req,res)=>{
+app.get("/home/new",asyncWrap(async(req,res)=>{
     res.render("new.ejs");
-})
+}));
 
 //show route
-app.get("/home/:id",async(req,res)=>{
+app.get("/home/:id",asyncWrap(async(req,res)=>{
     let{id}=req.params;
     let allVideos=await Video.find({});
     let showVideo= await Video.findById(id);
     res.render("show.ejs",{showVideo, allVideos});
-})
-
-
+}));
 
 //post route for upload
-app.post("/home",async(req,res)=>{
+app.post("/home",asyncWrap(async(req,res)=>{
     let newVideo= new Video(req.body.new);
     await newVideo.save().then((res)=>{console.log(res)});
     res.redirect("/home");
-});
+}));
 
 //Edit route
-app.get("/home/edit/:id",async(req,res)=>{
+app.get("/home/edit/:id",asyncWrap(async(req,res)=>{
     let {id}= req.params;
    let videoDetails= await Video.findById(id);
+   if(!videoDetails){
+    throw new ExpressError("Video not found",404);
+   };
     res.render("edit.ejs",{videoDetails});
-});
+}));
 
-app.put("/home/:id",async(req,res)=>{
+app.put("/home/:id",asyncWrap(async(req,res)=>{
     let {id}= req.params;
     let updated=await Video.findByIdAndUpdate(id,{...req.body.edit});
     console.log(updated);
     res.redirect("/home");
-});
+}));
 
 //Destroy Route
-app.delete("/home/:id",async(req,res)=>{
+app.delete("/home/:id",asyncWrap(async(req,res)=>{
     let {id}= req.params;
     let deletedVideo= await Video.findByIdAndDelete(id);
     console.log(deletedVideo);
     res.redirect("/home");
-});
+}));
 
 //Errror handling middleware
 app.use((err,req,res,next)=>{
